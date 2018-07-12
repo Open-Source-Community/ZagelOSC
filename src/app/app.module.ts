28e0a30091cc0daea7af0f16040d0939f0bcc70b
amrtaher1234 <amrtaher1995@gmail.com>
2018-07-12 23:07:30 +0200

    removed the auto update and added a manual update in the settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { MyApp } from './app.component';
 import { HttpClientModule  } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
-
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,6 +19,31 @@ import { EmoteProvider } from '../providers/emote/emote';
 
 import {LocalNotifications} from '@ionic-native/local-notifications'
 import { LoginPage } from '../pages/login/login';
+
+Pro.init('6C12FFFB', {
+  appVersion: '0.0.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -41,7 +67,8 @@ import { LoginPage } from '../pages/login/login';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }],
     RestfulProvider,
     EmoteProvider,
     LocalNotifications
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, App } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, App, ToastController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
-
+import {Pro} from '@ionic/pro';
 /**
  * Generated class for the SettingsPage page.
  *
@@ -16,8 +16,12 @@ import { LoginPage } from '../login/login';
 })
 export class SettingsPage {
 
+  public deployChannel = "";
+  public isBeta = false;
+  public downloadProgress = 0;
   coloring : string; 
   constructor(
+    public toastCtrl : ToastController,
     public app : App,
     public modalCtrl : ModalController, 
     public navCtrl: NavController,
@@ -49,6 +53,57 @@ export class SettingsPage {
   logout(){
     localStorage.clear(); 
     this.app.getRootNav().setRoot(LoginPage);   
-  
+
   }
+
+  async performManualUpdate() {
+
+    
+    /*
+      Here we are going through each manual step of the update process:
+      Check, Download, Extract, and Redirect.
+      This code is currently exactly the same as performAutomaticUpdate,
+      but you could split it out to customize the flow.
+
+      Ex: Check, Download, Extract when a user logs into your app,
+        but Redirect when they logout for an app that is always running
+        but used with multiple users (like at a doctors office).
+    */
+
+    try {
+      const haveUpdate = await Pro.deploy.check();
+
+      if (haveUpdate){
+        this.downloadProgress = 0;
+
+        await Pro.deploy.download((progress) => {
+          this.downloadProgress = progress;
+        })
+        await Pro.deploy.extract();
+         Pro.deploy.redirect().then((done)=>{
+           const toast = this.toastCtrl.create({
+             message : "Update is complete", 
+             duration : 1800
+           }); 
+           toast.present(); 
+           toast.onDidDismiss(function(){
+            this.app.getRootNav().setRoot("SplashPage");   
+           })
+         });
+      }
+      else {
+        this.toastCtrl.create({
+          message: "No updates found bud", 
+          duration: 2000 ,
+        }).present(); 
+      }
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+      // Pro.monitoring.exception(err);
+    }
+
+  }
+
 }
